bab5549e34ad421c9591c4c0165da66739375535
Hashim Aftab <hashimaftab5@gmail.com>
2025-09-09 16:04:09 +0500

    Enhance CSS styles across multiple files for improved layout, responsiveness, and visual consistency.

--- public/js/program.js
+++ public/js/program.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+	// Expand the first program section by default
+	$('.oneProgram').first().find('.oneProgramContent').show();
+	$('.oneProgram').first().find('#expandProgram').addClass("hide");
+	$('.oneProgram').first().find('#collapseProgram').removeClass("hide");
 
 	$('.oneProgramHeader').click(function() {
 		$(this).next('.oneProgramContent').slideToggle(600);
