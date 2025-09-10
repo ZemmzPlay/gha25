0451e8ffb336c2188757b58024c52ee1035c12bf
Bilawal Arif <bilawalarif003@gmail.com>
2025-09-10 20:33:26 +0500

    fix svg upload issue

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -69,10 +69,18 @@ class SettingsController extends Controller
     public function saveImageSettings($image, $imageName, $width, $height)
     {
         $image_name = $imageName.'_'.time() . '.' . $image->getClientOriginalExtension();
-        $imageToSave = Image::make($image->getRealPath());
-        $imageToSave->resize($width, $height, function ($constraint) {
-            $constraint->aspectRatio();
-        })->save('images/' . $image_name);
+        
+        // Handle SVG files differently - no resizing needed
+        if ($image->getClientOriginalExtension() === 'svg') {
+            $image->move('images/', $image_name);
+        } else {
+            // Handle raster images (jpg, png, gif) with resizing
+            $imageToSave = Image::make($image->getRealPath());
+            $imageToSave->resize($width, $height, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save('images/' . $image_name);
+        }
+        
         return $image_name;
     }
 }
